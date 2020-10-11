56e2128048c8f69f10d0c3a5fc46249936f7f871
Robson Zagre Junior <38474282+robsonzagrejr@users.noreply.github.com>
2020-10-11 17:10:22 -0300

    a

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anp_city_extract',
+    version='1.0.0',
+    url='https://github.com/robsonzagrejr/anp_city_extract',
+    author='Robson Zagre Júnior',
+    author_email='r.zagre.jr@gmail.com',
+    description='Simple ANP info stract for UFSC Probabilidade e Estatistica class'
+    packages=find_packages(),    
+)
